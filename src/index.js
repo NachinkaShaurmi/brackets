b4d7303fe855a958ef810b8dffd5cf65e8c70eb1
Yuriy <agent250691@yandex.ru>
2020-09-15 16:36:16 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let count = Array(bracketsConfig.length).fill(0);
+    
+    for (let indArr = 0; indArr < bracketsConfig.length; indArr++) {
+        for (let j = 0; j < str.length; j++) {    
+            if (str[j] === bracketsConfig[indArr][0]) count[indArr]++;
+            if (str[j] === bracketsConfig[indArr][1]) {
+                count[indArr]--;
+                for (let i = indArr; i < count.length; i++) {
+                   if (count[i] !== 0) return false; 
+                }
+            }
+            for (let a of count) {
+                if (a < 0) return false;
+            }
+        }
+    }
+    for (let a of count) {
+        if (a !== 0) return false;
+    }
+    return true;
+    // for (let indArr = 0; indArr < bracketsConfig.length; indArr++) {  
+    //   for (let j = 0; j < str.length; j++) {
+    //       if (str[j] === bracketsConfig[indArr][0]) count[indArr]++;
+    //       if (str[j] === bracketsConfig[indArr][1]) count[indArr]--;
+    //       for (let a of count) {
+    //           if (a < 0) return false;
+    //       }
+    //   }
+    // i      
+    // return true;
+
 }
